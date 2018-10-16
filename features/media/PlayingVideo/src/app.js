1ad77fe39e44a93f3ae6ce18a83ac2183a339bcb
Joshua Granick <jgranick@users.noreply.github.com>
2018-10-16 16:01:43 -0700

    Update samples

--- features/media/PlayingVideo/src/app.js
+++ features/media/PlayingVideo/src/app.js
@@ -1,7 +1,10 @@
 "use strict";
+var Actuate = require ("motion/Actuate").default;
 var Sprite = require ("openfl/display/Sprite").default;
 var Stage = require ("openfl/display/Stage").default;
 var AsyncErrorEvent = require ("openfl/events/AsyncErrorEvent").default;
+var MouseEvent = require ("openfl/events/MouseEvent").default;
+var NetStatusEvent = require ("openfl/events/NetStatusEvent").default;
 var Video = require ("openfl/media/Video").default;
 var NetConnection = require ("openfl/net/NetConnection").default;
 var NetStream = require ("openfl/net/NetStream").default;
@@ -20,7 +23,14 @@ var App = function () {
 	this.netStream = new NetStream (netConnection);
 	this.netStream.client = { onMetaData: this.client_onMetaData.bind (this) };
 	this.netStream.addEventListener (AsyncErrorEvent.ASYNC_ERROR, this.netStream_onAsyncError.bind (this)); 
-	this.netStream.play ("assets/example.mp4");
+	
+	this.overlay = new Sprite ();
+	this.overlay.graphics.beginFill (0, 0.5);
+	this.overlay.graphics.drawRect (0, 0, 560, 320);
+	this.overlay.addEventListener (MouseEvent.MOUSE_DOWN, this.overlay_onMouseDown.bind (this));
+	this.addChild (this.overlay);
+	
+	netConnection.addEventListener (NetStatusEvent.NET_STATUS, this.netConnection_onNetStatus.bind (this));
 	
 }
 
@@ -40,6 +50,25 @@ App.prototype.client_onMetaData = function (metaData) {
 App.prototype.netStream_onAsyncError = function (event) {
 	
 	console.error ("Error loading video");
+	
+}
+
+
+App.prototype.netConnection_onNetStatus = function (event) {
+		
+	if (event.info.code == "NetStream.Play.Complete") {
+		
+		Actuate.tween (this.overlay, 1, { alpha: 1 });
+		
+	}
+	
+}
+
+
+App.prototype.overlay_onMouseDown = function (event) {
+	
+	Actuate.tween (this.overlay, 2, { alpha: 0 });
+	this.netStream.play ("assets/example.mp4");
 	
 }
 
